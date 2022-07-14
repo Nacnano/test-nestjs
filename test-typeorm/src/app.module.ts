54c9f7f2aa4645741d042f5744986119716befd4
nac <chotpisit080916c@gmail.com>
2022-07-14 17:05:13 +0700

    setup database

--- test-typeorm/src/app.module.ts
+++ test-typeorm/src/app.module.ts
@@ -1,18 +1,21 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersModule } from "./users/user.module";
+import { User } from "./users/user.entity";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
+      type: "mysql",
+      host: "localhost",
       port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
-      entities: [],
+      username: "root",
+      password: "root",
+      database: "test",
+      entities: [User],
       synchronize: true,
     }),
+    UsersModule,
   ],
 })
 export class AppModule {}
