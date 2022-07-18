2f45ae31ae0e4f859c918ca90a5bbd6bcec2e652
nac <chotpisit080916c@gmail.com>
2022-07-18 14:06:23 +0700

    setup new config and add new environment

--- test-typeorm/src/app.module.ts
+++ test-typeorm/src/app.module.ts
@@ -4,15 +4,16 @@ import { UsersModule } from "./users/users.module";
 import { User } from "./users/user.entity";
 import { ConfigModule } from "@nestjs/config";
 ConfigModule.forRoot({ envFilePath: ".env" });
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: "mysql",
-      host: "localhost",
-      port: 3306,
-      username: "root",
-      password: "root",
-      database: "test",
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [User],
       synchronize: true,
       autoLoadEntities: true,
