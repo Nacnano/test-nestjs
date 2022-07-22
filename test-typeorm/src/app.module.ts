6fa784c826c3ca59bfd7a3edd727042b7e17f887
nac <chotpisit080916c@gmail.com>
2022-07-22 10:41:19 +0700

    fix(test-typeorm): fix environment file setup

--- test-typeorm/src/app.module.ts
+++ test-typeorm/src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UsersModule } from "./users/users.module";
 import { User } from "./users/user.entity";
+import { ConfigModule } from "@nestjs/config";
+ConfigModule.forRoot({ envFilePath: ".env" });
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: "mysql",
       host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT),
+      port: parseInt(process.env.DATABASE_PORT) || 3306,
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
